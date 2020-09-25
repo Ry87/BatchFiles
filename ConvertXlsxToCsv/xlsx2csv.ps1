6eaa46196ee0b9fb2298bca8cfe1bfea6cd8f8d9
RyHana <55495694+Ry87@users.noreply.github.com>
2020-09-26 03:31:38 +0900

    Add xlsx2csv.ps1
    This file converts sheets in xlsx to csv files.
    The user have to input the xlsx file path to the top line.

--- ConvertXlsxToCsv/xlsx2csv.ps1
+++ ConvertXlsxToCsv/xlsx2csv.ps1
@@ -1,1 +1,22 @@
-powershell -NoProfile -ExecutionPolicy Unrestricted .\xlsx2csv.ps1
+$tmp = "ここにxlsxファイルのパス"
+
+$objExcel = New-Object -ComObject Excel.Application
+
+
+$path = (resolve-path -path $tmp).path
+
+Start-Sleep 1
+
+$i = 0
+$objworkbook=$objExcel.Workbooks.Open($tmp)
+
+foreach($sh in $objworkbook.Sheets){
+  $sh.Select()
+  $xlsxName = [string]$i + ".csv"
+  $savePath = $tmp -replace ".xlsx",$xlsxName
+  $objworkbook.SaveAs($savePath, 6)
+  $i += 1
+}
+
+$objworkbook.Close($false)
+$objExcel.Quit()
